b0eb9abaf6d798105cc6bc077103b25b0bf74bf9
Yoni TOUITOU <yonitouitou@yahoo.fr>
2012-12-09 20:28:58 +0200

    RED = test for 300

--- romanNumeralsConvertion/src/romanNumeralUnitTests.java
+++ romanNumeralsConvertion/src/romanNumeralUnitTests.java
@@ -12,6 +12,7 @@ public class romanNumeralUnitTests {
 		assertEquals(false, n.checkInput(-3));
 		assertEquals("I", n.NumberToRomain(1));
 		assertEquals("LX", n.NumberToRomain(60));
+		assertEquals("CCC", n.NumberToRomain(300));
 	}
 
 }
