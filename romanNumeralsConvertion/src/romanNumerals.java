3d9110188def5d146c4ff06a58d27f20a7074a7a
Yoni TOUITOU <yonitouitou@yahoo.fr>
2012-12-09 20:22:35 +0200

    GREEN = test for 1

--- romanNumeralsConvertion/src/romanNumerals.java
+++ romanNumeralsConvertion/src/romanNumerals.java
@@ -11,7 +11,23 @@ public class romanNumerals {
 	 {
 	      String s;
 	      s = "";
+	       switch(x % 10)
+	       {
+	       		case 9: s+="IX";break;
+				case 8: s+="VIII";break;
+				case 7: s+="VII";break;
+				case 6: s+="VI";break;
+				case 5: s+="V";break;
+				case 4: s+="IV";break;
+				case 3: s+="III";break;
+				case 2: s+="II";break;
+				case 1: s+="I";break;
+	       }
+	      
+	      
+	      
 	      return s;
+	      
 	 }
 	 
 }
